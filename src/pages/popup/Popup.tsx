77b9a24e46e34c6bf7b30ae4e64185e05aeb70de
Julian Gödde <goedde@tuta.io>
2023-10-27 18:11:30 +0200

    Add additional user info to popup when authorized
    The Popup component was updated to display additional user information when `data.isAuthorized` is `true`. Added `Avatar`, `Divider`, and `Table` components from `@mantine/core` and `IconBrandAuth0`, `IconUserScan` from `@tabler/icons-react` to enhance the visual representation of user's data. This update aims to give a more comprehensive overview of the user's account information straight from the popup, thus improving user experience and making the application more intuitive.

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -3,9 +3,10 @@ import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import { NoChaynsEnvFound } from '@pages/popup/NoChaynsEnvFound';
 import { useChaynsEnvData } from '@pages/popup/useChaynsEnvData';
-import { Anchor, Center, Table, Title } from '@mantine/core';
+import { Anchor, Avatar, Center, Divider, Group, PasswordInput, Table, Title } from '@mantine/core';
 import { WaitingForData } from '@pages/popup/WaitingForData';
 import { CopyableDataRow } from '@pages/popup/CopyableDataRow';
+import { IconBrandAuth0, IconUserScan } from '@tabler/icons-react';
 
 const Popup = () => {
   const { data, isWaiting } = useChaynsEnvData();
@@ -32,6 +33,29 @@ const Popup = () => {
           <CopyableDataRow label={'Domain'} value={data.domain} />
         </Table.Tbody>
       </Table>
+
+      {data.isAuthorized && (
+        <>
+          <Divider
+            my="xs"
+            size={'lg'}
+            label={
+              <Group>
+                <Avatar src={'https://sub60.tobit.com/u/' + data.personId + '?size=300'} radius={'sm'} />{' '}
+                {data.firstName} {data.lastName}
+              </Group>
+            }
+            labelPosition="center"
+          />
+          <Table>
+            <Table.Tbody>
+              <CopyableDataRow label={'PersonId'} value={data.personId} />
+              <CopyableDataRow label={'UserId'} value={data.tobitUserId} />
+              <CopyableDataRow label={'Access Token'} value={data.tobitAccessToken} />
+            </Table.Tbody>
+          </Table>
+        </>
+      )}
     </div>
   );
 };
