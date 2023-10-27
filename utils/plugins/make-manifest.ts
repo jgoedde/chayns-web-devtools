271d7cef6cbc9b0a9ad6c1450cfdeed692a39e46
Julian Gödde <goedde@tuta.io>
2023-10-27 12:47:37 +0200

    Remove cache invalidation key replacement
    Removed cache invalidation mechanism that replaced keys in content script CSS path. This was removed because caching is now handled differently, making this way of invalidating cache obsolete. This change improves maintainability as the codebase is cleaner and easier to understand.

--- utils/plugins/make-manifest.ts
+++ utils/plugins/make-manifest.ts
@@ -19,13 +19,6 @@ export default function makeManifest(
     }
     const manifestPath = resolve(to, 'manifest.json');
 
-    // Naming change for cache invalidation
-    if (config.contentScriptCssKey) {
-      manifest.content_scripts.forEach(script => {
-        script.css = script.css.map(css => css.replace('<KEY>', config.contentScriptCssKey));
-      });
-    }
-
     fs.writeFileSync(manifestPath, ManifestParser.convertManifestToString(manifest));
 
     colorLog(`Manifest file copy complete: ${manifestPath}`, 'success');
