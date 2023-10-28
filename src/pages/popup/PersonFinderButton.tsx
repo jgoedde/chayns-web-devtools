84094713f7e5372b94e2b9c3b85eb15b140639c8
Julian Gödde <goedde@tuta.io>
2023-10-28 09:38:58 +0200

    Improve PersonFinder UI and add margin to TextInput.
    The PersonFinderButton.tsx file was modified to improve the Person Finder modal's user interface. This incorporated wrapping the TextInput field within a Box. Also, a bottom margin was applied to the Box enclosing the TextInput to add a little more spacing in the modal dialog. This visual tweak enhances the legibility and improves the overall user experience in the modal.

--- src/pages/popup/PersonFinderButton.tsx
+++ src/pages/popup/PersonFinderButton.tsx
@@ -1,4 +1,4 @@
-import { Accordion, Alert, Avatar, Button, Center, Group, Modal, Table, Text, TextInput } from '@mantine/core';
+import { Accordion, Alert, Avatar, Box, Button, Center, Group, Modal, Table, Text, TextInput } from '@mantine/core';
 import { IconInfoCircle, IconUserSearch } from '@tabler/icons-react';
 import { useDebouncedValue, useDisclosure } from '@mantine/hooks';
 import React, { useState } from 'react';
@@ -27,44 +27,46 @@ export function PersonFinderButton() {
   return (
     <>
       <Modal opened={opened} onClose={close}>
-        <TextInput
-          label={'Person suchen'}
-          placeholder={'PersonId, UserId, Vorname, Nachname'}
-          value={value}
-          onChange={e => setValue(e.currentTarget.value)}
-        />
-        {error != null ? (
-          <Alert variant={'light'} color={'red'} title={'Personen'} icon={<IconInfoCircle />}>
-            Die Liste der Personen konnte nicht abgerufen werden. Vermutlich ist dein Access Token abgelaufen.
-          </Alert>
-        ) : (
-          <>
-            {isLoading ? (
-              <CenteredLoader />
-            ) : (
-              <Accordion>
-                {relations.map(p => (
-                  <Accordion.Item key={p.personId} value={p.personId}>
-                    <Accordion.Control>
-                      <AccordionLabel
-                        label={`${p.firstName} ${p.lastName}`}
-                        image={`https://sub60.tobit.com/u/${p.userId}?size=100`}
-                      />
-                    </Accordion.Control>
-                    <Accordion.Panel>
-                      <Table>
-                        <Table.Tbody>
-                          <CopyableDataRow label={'PersonId'} value={p.personId} />
-                          <CopyableDataRow label={'UserId'} value={p.userId} />
-                        </Table.Tbody>
-                      </Table>
-                    </Accordion.Panel>
-                  </Accordion.Item>
-                ))}
-              </Accordion>
-            )}
-          </>
-        )}
+        <Box mb={'md'}>
+          <TextInput
+            label={'Person suchen'}
+            placeholder={'PersonId, UserId, Vorname, Nachname'}
+            value={value}
+            onChange={e => setValue(e.currentTarget.value)}
+          />
+          {error != null ? (
+            <Alert variant={'light'} color={'red'} title={'Personen'} icon={<IconInfoCircle />}>
+              Die Liste der Personen konnte nicht abgerufen werden. Vermutlich ist dein Access Token abgelaufen.
+            </Alert>
+          ) : (
+            <>
+              {isLoading ? (
+                <CenteredLoader />
+              ) : (
+                <Accordion>
+                  {relations.map(p => (
+                    <Accordion.Item key={p.personId} value={p.personId}>
+                      <Accordion.Control>
+                        <AccordionLabel
+                          label={`${p.firstName} ${p.lastName}`}
+                          image={`https://sub60.tobit.com/u/${p.userId}?size=100`}
+                        />
+                      </Accordion.Control>
+                      <Accordion.Panel>
+                        <Table>
+                          <Table.Tbody>
+                            <CopyableDataRow label={'PersonId'} value={p.personId} />
+                            <CopyableDataRow label={'UserId'} value={p.userId} />
+                          </Table.Tbody>
+                        </Table>
+                      </Accordion.Panel>
+                    </Accordion.Item>
+                  ))}
+                </Accordion>
+              )}
+            </>
+          )}
+        </Box>
       </Modal>
       <Center>
         <Button onClick={open} variant={'subtle'} rightSection={<IconUserSearch size={14} />}>
